6f221042c1a4949f15390872397874d4e2b7bd68
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-11 15:56:06 +0200

    add all CRUD operations

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 // const express = require("express"); -> normal commonjs, when not using TS
 
-import { createTodo } from "../controllers/todos";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todos";
 
 const router = Router();
 
 router.post("/", createTodo);
-router.get("/");
-router.patch("/:id");
-router.delete("/:id");
+router.get("/", getTodos);
+router.patch("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 export default router;
